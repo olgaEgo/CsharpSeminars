8b9cc5c8eacc477f07802088f4ef4bb8892005ba
OlgaKoroleva <olga.s.egorova@gmail.com>
2022-09-15 22:59:47 +0300

    modified Task04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -5,3 +5,22 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (max < number2)
+{
+    max = number2;
+}
+if (max < number3)
+{
+     max = number3;
+}
+
+Console.WriteLine(max);
