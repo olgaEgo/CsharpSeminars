4d9e67f159c7bf0d62e8761ca5baa31b46aeb209
OlgaKoroleva <olga.s.egorova@gmail.com>
2022-09-15 22:11:57 +0300

    add Task 04

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,14 +1,15 @@
-﻿// Напишите команду, которая на вход принимает 
-// одно число (N), а на выходе показывает все целые
-// числа в промежутке от -N до N.
-// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
-// 2 -> "-2, -1, 0, 1, 2"
+﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
 
-Console.Write("Введите целое число: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int first = -N;
-while (first <= N)
-{
-    Console.Write($"{first} ");
-    first++;
-}
+Console.Write("Введите целое число от 1 до 7: ");
+string number = Console.ReadLine();
+if (number == "1") Console.WriteLine("Monday");
+else if (number == "2") Console.WriteLine("Tuesday");
+else if (number == "3") Console.WriteLine("Wensday");
+else if (number == "4") Console.WriteLine("Thirsday");
+else if (number == "5") Console.WriteLine("Friday");
+else if (number == "6") Console.WriteLine("Saturday");
+else if (number == "7") Console.WriteLine("Sunday");
+else Console.WriteLine("Введите верный номер");
