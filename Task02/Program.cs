85c793556e563f92892ea28e1994c11b878905f8
OlgaKoroleva <olga.s.egorova@gmail.com>
2022-09-15 21:50:16 +0300

    modified Task03

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,21 +1,15 @@
-﻿// Напишите программу, которая на вход принимает два
-// числа и проверяет, является ли первое число квадратом
-// второго.
-// a = 25, b = 5 -> да
-// a = 2, b = 10 -> нет
-// a = 9, b = -3 -> да
-// a = -3 b = 9 -> нет
+﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
 
-Console.Write("Введите первое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
-int square = number2 * number2;
-if (number1 == square)
-{
-    Console.WriteLine("да");
-}
-else
-{
-    Console.WriteLine("нет");
-}
+Console.Write("Введите целое число от 1 до 7: ");
+string number = Console.ReadLine();
+if (number == "1") Console.WriteLine("Monday");
+else if (number == "2") Console.WriteLine("Tuesday");
+else if (number == "3") Console.WriteLine("Wensday");
+else if (number == "4") Console.WriteLine("Thirsday");
+else if (number == "5") Console.WriteLine("Friday");
+else if (number == "6") Console.WriteLine("Saturday");
+else if (number == "7") Console.WriteLine("Sunday");
+else Console.WriteLine("Введите верный номер");
