9b07c30a94385ecac79aa6353a68b445aee00fd5
Olga Koroleva <olga@Yurys-iMac.lan>
2022-10-07 22:57:48 +0300

    mod Task41, Task43

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -3,31 +3,63 @@
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
 
+Console.Clear();
 Console.Write("Сообщите количество вводимых чисел: ");
 int size = Convert.ToInt32(Console.ReadLine());
 
-int[] FillArrayUsersDigits(int size)
+double[] FillArrayUsersDigits(int size)
 {
-    int[] array = new int[size];
-
-    for (int i = 0; i < array.Length; i++) //array.Length
+    double[] array = new double[size];
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
     {
-        System.Console.Write($"Введите {i+1} число: ");
+        Console.Write($"Введите {i+1} число: ");
         array[i] = Convert.ToInt32(Console.ReadLine());
     }
     return array;
 }
 
-int[] arr = FillArrayUsersDigits(size);
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.WriteLine("]");
+}
 
-int DigitsOverZero(int[] array)
+double[] listOfNumbers = FillArrayUsersDigits(size);
+PrintArray(listOfNumbers);
+
+int DigitsOverZero(double[] array)
 {
     int count = default;
-    for (int i = 0; i < array.Length; i++)
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
     {
         if (array[i] > 0) count++;
     }
     return count;
 }
 
-Console.WriteLine($"Количество чисел больше '0' - {DigitsOverZero(arr)}");
+Console.WriteLine($"Количество чисел больше '0' - {DigitsOverZero(listOfNumbers)}");
+
+// int[] FillArrayUsersDigits(int size)
+// {
+//     int[] array = new int[size];
+
+//     for (int i = 0; i < array.Length; i++) //array.Length
+//     {
+//         System.Console.Write($"Введите {i+1} число: ");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
--- Task43/Program.cs
+++ Task43/Program.cs
@@ -4,26 +4,47 @@
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 Console.Write("Введите b1: ");
-double b1 = Convert.ToDouble(Console.ReadLine());
+double b1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите k1: ");
-double k1 = Convert.ToDouble(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите b2: ");
-double b2 = Convert.ToDouble(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите k2: ");
-double k2 = Convert.ToDouble(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
 
-double x = Math.Round((b1 - b2) / (k2 - k1), 1);
-double y = Math.Round(k1 * x + b1, 1);
-
-if (k1 == k2 && b1 == b2)
-{
-    Console.WriteLine($"Прямые совпадают между собой.");
-}
+if (k1 == k2 && b1 == b2) Console.WriteLine($"Прямые совпадают между собой.");
 else if (k1 == k2)
 {
     Console.WriteLine($"Прямые параллельны между собой.");
 }
-else  if (k1 * x + b1 == k2 * x + b2)
+else
 {
-    Console.WriteLine($"({x}, {y})");
-}    
+    double[] coordinates = CoordinatesOfIntersectionOfStraightLines(b1, k1, b2, k2);
+    PrintArray(coordinates);
+}
+
+double[] CoordinatesOfIntersectionOfStraightLines(double b1, double k1, double b2, double k2)
+{
+    double x = Math.Round((b1 - b2) / (k2 - k1), 1);
+    double y = Math.Round(k1 * x + b1, 1);
+    double[] array = new double[] { x, y };
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.WriteLine("]");
+}
