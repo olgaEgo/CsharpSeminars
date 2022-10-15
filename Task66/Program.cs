eacc450aa042c97a9a1a075df837dafff6483202
Olga Koroleva <olga@Yurys-iMac.lan>
2022-10-15 22:55:07 +0300

    Add DZ9

---
+++ Task66/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+if (numberM < 1 && numberN < 1) Console.Write("Введены два ненатуральных числа.");
+else
+{
+    if (numberM > numberN)
+{
+    (numberM, numberN) = (numberN, numberM);
+}
+
+int sum = SumOfNaturalNumbersFromMToN(numberM, numberN);
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {numberM} до {numberN} равна {sum}.");
+
+int SumOfNaturalNumbersFromMToN(int numM, int numN)
+{
+    int sum = numM;
+
+    if (numN > numM && numM > 0) sum += SumOfNaturalNumbersFromMToN(numM + 1, numN);
+    // if (numM > numN && numN > 0) sum += SumOfNaturalNumbersFromMToN(numM - 1, numN);
+
+    if (numN > numM && numM < 0 && numN > 0)
+    {
+        sum = 0;
+        numM = 0;
+        sum += SumOfNaturalNumbersFromMToN(numM + 1, numN);
+    }
+
+//     if (numM > numN && numN < 0 && numM > 0)
+//     {
+//         sum = 0;
+//         numN = 0;
+//         sum += SumOfNaturalNumbersFromMToN(numN + 1, numM);
+//     }
+     return sum;
+}
+}
+
---
+++ Task68/Program.cs
@@ -0,0 +1,25 @@
+﻿// адача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m < 0 || n < 0)  Console.Write("Введите два неотрицательных числа.");
+else
+{
+    int akk = Akkerman(m, n);
+    Console.WriteLine(akk);
+}
+
+int Akkerman(int m, int n)
+{
+    int akkerman = 0;
+    if (m == 0) akkerman = n + 1;
+    if (m > 0 && n == 0) akkerman = Akkerman(m - 1, 1);
+    if (m > 0 && n > 0) akkerman = Akkerman(m - 1, Akkerman(m, n - 1));
+    return akkerman;
+}
